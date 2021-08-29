9b87aec710b9f9c5027c2b1fdb59c66293e01646
Mak8890 <68355032+Mak8890@users.noreply.github.com>
2021-08-29 14:58:01 +0400

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eTicket.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "eTicket description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "eTicket contact page.";
+
+            return View();
+        }
+    }
+}
